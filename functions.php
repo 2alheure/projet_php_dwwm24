f97ccca0db613024c79241ce76cf57629a99142a
2alheure <jojo77.juv@live.fr>
2022-02-24 10:08:27 +0100

    Cacher les informations sensibles

--- functions.php
+++ functions.php
@@ -7,13 +7,14 @@ function url(string $route) {
 function seConnecter() {
     // Include the Simple ORM class
     require_once __DIR__ . '/models/SimpleOrm.php';
+    require_once __DIR__ . '/config.php';
 
     // Connect to the database using mysqli
-    $conn = new mysqli('localhost', 'root', '');
+    $conn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD);
 
     if ($conn->connect_error)
         die(sprintf('Unable to connect to the database. %s', $conn->connect_error));
 
     // Tell Simple ORM to use the connection you just created.
-    SimpleOrm::useConnection($conn, 'projet_php');
+    SimpleOrm::useConnection($conn, DATABASE_NAME);
 }
